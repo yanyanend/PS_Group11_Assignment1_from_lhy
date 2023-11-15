b151c1ade524b9212821374ab3e45574f294c1b8
MarcoLius <67773721+MarcoLius@users.noreply.github.com>
2023-11-15 00:48:05 +0000

    Delete cmake-build-release-gcc directory
    Delete some local directory

--- cmake-build-release-gcc/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Volumes/T7/Software Files/Git Projects/Ps_Group11_Assignment1/cmake-build-release-gcc/_deps/googletest-src/googlemock
-# Build directory: /Volumes/T7/Software Files/Git Projects/Ps_Group11_Assignment1/cmake-build-release-gcc/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
